6eeb7cb5df20b4914f41a4171da2bf58e97ee95d
Moritz Frisch <mo68@posteo.net>
2021-07-07 18:25:11 +0200

    Initial Commit

---
+++ moForth/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  moForth
+//
+//  Created by Moritz Frisch on 07.07.21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
